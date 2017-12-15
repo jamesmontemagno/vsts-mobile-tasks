437b88e2d66e42001ed232d0ea38d3aaa27e2ee1
James Montemagno <james.montemagno@gmail.com>
2017-12-15 11:41:09 -0800

    Implement Android in Typescript

---
+++ tasks/AndroidBumpVersion/task.ts
@@ -0,0 +1,79 @@
+import tl = require('vsts-task-lib/task');
+import trm = require('vsts-task-lib/toolrunner');
+import fs = require('fs');
+import library = require('samchon-framework').library;
+import { isNullOrUndefined } from 'util';
+
+async function run() {
+    try {
+        let tool: trm.ToolRunner;
+        if (process.platform == "win32") {
+            tl.setResult(tl.TaskResult.Failed, "Task not supported");
+            return;
+        }
+
+        let sourcePath: string = tl.getInput("sourcePath");
+        let versionCodeOffset: String = tl.getInput("versionCodeOffset");
+        let versionCode: String = tl.getInput("versionCode");
+        let versionName: String = tl.getInput("versionName");
+        let printFile: Boolean = new Boolean(tl.getInput("printFile")).valueOf();
+        
+        console.log(' (i) Provided Info.plist path:' + sourcePath);
+
+        if(!isNullOrUndefined(versionName))
+        {
+            console.log(' (i) Version Name (shortcode): ' + versionName);
+        }
+
+        if(!isNullOrUndefined(versionCodeOffset))
+        {
+            console.log(' (i) versionCodeOffset: ' + versionCodeOffset);
+            versionCode = String(Number(versionCode)/1 + Number(versionCodeOffset)/1);
+        } 
+
+        console.log(' (i) versionCode: ' + versionCode);
+
+        if(!isNullOrUndefined(printFile))
+        {
+            console.log('Original info.Plist:' + xmlString);
+        }
+
+        
+        let xmlString: String = fs.readFileSync(sourcePath, 'utf8');
+        let xml: library.XML = new library.XML(xmlString);
+
+        //Update package name here
+        console.log( xml.has("manifest") ); // true
+        let manifestNode: library.XMLList = xml.get("manifest");
+        console.log("Old versionCode: " + manifestNode.at(0).getProperty("android:versionCode") );
+
+        manifestNode.at(0).setProperty("android:versionCode", versionCode);
+
+        console.log("New versionCode: " + manifestNode.at(0).getProperty("android:versionCode") );
+        
+
+        if(!isNullOrUndefined(versionName))
+        {
+            console.log("Old versionName: " + manifestNode.at(0).getProperty("android:versionName") );
+
+            manifestNode.at(0).setProperty("android:versionName", versionName);
+
+            console.log("New versionName: " + manifestNode.at(0).getProperty("android:versionName") );
+        }
+
+
+        if(!isNullOrUndefined(printFile))
+        {
+            // todo - load plist data
+            console.log('Final info.Plist: ' + fs.readFileSync(sourcePath, 'utf8'));
+        }
+        
+    
+        console.log('Task done!');
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
---
+++ tasks/AndroidPackageName/task.ts
@@ -0,0 +1,73 @@
+import tl = require('vsts-task-lib/task');
+import trm = require('vsts-task-lib/toolrunner');
+import fs = require('fs');
+import library = require('samchon-framework').library;
+import { isNullOrUndefined } from 'util';
+
+async function run() {
+    try {
+        let tool: trm.ToolRunner;
+        if (process.platform == "win32") {
+            tl.setResult(tl.TaskResult.Failed, "Task not supported");
+            return;
+        }
+
+        let sourcePath: string = tl.getInput("sourcePath");
+        let packageName: String = tl.getInput("packageName");
+        let appLabel: String = tl.getInput("appLabel");
+        let printFile: Boolean = new Boolean(tl.getInput("printFile")).valueOf();
+        
+        // requires parameters
+        if(!isNullOrUndefined(sourcePath))
+        {
+            throw new Error("[!] Missing required input: sourcePath");
+        }
+
+        let xmlString: String = fs.readFileSync(sourcePath, 'utf8');
+        let xml: library.XML = new library.XML(xmlString);
+
+
+        if(!isNullOrUndefined(printFile))
+        {
+            console.log('Original info.Plist:' + xmlString);
+        }
+
+        //Update package name here
+        console.log( xml.has("manifest") ); // true
+        let manifestNode: library.XMLList = xml.get("manifest");
+        console.log("Old package: " + manifestNode.at(0).getProperty("package") );
+
+        manifestNode.at(0).setProperty("package", packageName);
+
+        console.log("New package: " + manifestNode.at(0).getProperty("package") );
+        
+
+        
+        //Update applabel is specified
+        if(!isNullOrUndefined(appLabel))
+        {
+            console.log( xml.has("application") ); // true
+            let applicationNode: library.XMLList = xml.get("application");
+            console.log("Old appLabel: " + applicationNode.at(0).getProperty("android:label") );
+
+            applicationNode.at(0).setProperty("android:label", appLabel);
+
+            console.log("New appLabel: " + applicationNode.at(0).getProperty("android:label") );
+        }
+
+        
+
+        if(!isNullOrUndefined(printFile))
+        {
+            // todo - load plist data
+            console.log('Final info.Plist: ' + fs.readFileSync(sourcePath, 'utf8'));
+        }
+        
+        console.log('Task done!');
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
